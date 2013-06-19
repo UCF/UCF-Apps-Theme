4044e37f87f710215b124a39dee2af96556e9aeb
csantillo90 <csantillo90@gmail.com>
2013-06-19 13:23:54 -0400

    style.css update

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 						</div>
 					</div>
 					<div class="row">
-						<div class="span12">
+						<div class="centerContent">
 							<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?>
 								<?php $options = get_option(THEME_OPTIONS_NAME);?>
 								<div class = "footerspan1">
