47f9161a847ddf8f9a05d388bea8dbebc56a0e24
csantillo90 <christopher.santillo@ucf.edu>
2013-06-21 16:13:13 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -69,6 +69,7 @@ class Category_Checklist {
 	// Scrolls to first checked category
 	function script() {
 ?>
+<!--
 <script type="text/javascript">
 	jQuery(function(){
 		jQuery('[id$="-all"] > ul.categorychecklist').each(function() {
@@ -85,6 +86,7 @@ class Category_Checklist {
 		});
 	});
 </script>
+-->
 <?php
 	}
 }
