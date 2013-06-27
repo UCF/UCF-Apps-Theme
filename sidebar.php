0492634c2549a92992d6cafc8a9198921eaedceb
Judomeades <judomeades@hotmail.com>
2013-06-27 12:40:58 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 	<?php $taxonomy_term = get_post_meta($post->ID, 'page_taxonomy_term', TRUE) ? get_post_meta($post->ID, 'page_taxonomy_term', TRUE) : $post->post_name; ?>
 	
 	<p class="post-search-header-text">Search This Section</p>
-	<?=do_shortcode('[search_form post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
+	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
 					
 
 <?php endif;?>
