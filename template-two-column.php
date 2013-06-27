697197d7831a7c9e062f1e77efef9d33389f187f
Judomeades <judomeades@hotmail.com>
2013-06-27 12:32:45 -0400

    Update template-two-column.php

--- template-two-column.php
+++ template-two-column.php
@@ -9,22 +9,22 @@
 			<div class="row page-content" id="<?=$post->post_name?>">
 				<div class="span12" id="page-top">
 					<article> 
-						<?//=get_breadcrumbs($post->ID)?>
-						<? //if(!is_front_page())	{ ?>
-							<h2><?php// the_title();?></h2>
+						<?=get_breadcrumbs($post->ID)?>
+						<? if(!is_front_page())	{ ?>
+							<h2><?php the_title();?></h2>
 						<? } ?>
 						
 						<?php if (get_post_meta($post->ID, 'page_subheader', TRUE)) {
 							print get_post_meta($post->ID, 'page_subheader', TRUE);
 						}
 						?>  
-				<!--	 </article> -->
+					 </article> 
 				</div>
 				<div id="sidebar" class="span3">
 					<?=get_sidebar();?>
 				</div>
 				<div class="span9">
-				<!--	<article> -->
+					<article>
 						<?php the_content();?>
 					</article>
 				</div>
