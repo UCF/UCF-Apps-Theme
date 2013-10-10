ff4c3a919cae7157144f6c34438d9378672f30b9
Craig Froehlich <craigf@outlook.com>
2013-10-10 09:53:22 -0400

    Style updates

--- functions/base.php
+++ functions/base.php
@@ -495,7 +495,7 @@ function bootstrap_menus() {
 				$item_output = $args->before;
 				$item_output .= '<a'. $attributes .'>';
 				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-				$item_output .= ($args->has_children) ? ' <i class="icon-circle-arrow-down"></i></a>' : '</a>';
+				$item_output .= ($args->has_children) ? ' <i class="icon-chevron-down icon-white"></i></a>' : '</a>';
 				$item_output .= $args->after;
 
 				$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
