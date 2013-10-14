417c7685d4a5c88e0344ea57c6a08a55244e051e
Craig Froehlich <craigf@outlook.com>
2013-10-14 11:19:34 -0400

    Fixed menu title bug in sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
 
 	<?php $side_menu = get_post_meta($post->ID, 'page_sidebar_menu', TRUE) ? get_post_meta($post->ID, 'page_sidebar_menu', TRUE) : $post->post_name; ?>
 	
-	<h2 class=widgettitle"><? $side_menu ?> </h2>
+	<h2 class=widgettitle"><? echo $side_menu ?> </h2>
 	<?=wp_nav_menu(array(
 	'theme_location' => '',
 	'menu' => $side_menu,
