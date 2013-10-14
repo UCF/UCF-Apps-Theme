a1a7b77266682f8e28ef8e820f643cff130af2c2
Craig Froehlich <craigf@outlook.com>
2013-10-14 10:59:08 -0400

    Adding menu header to sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,13 +4,13 @@
 
 	<?php $side_menu = get_post_meta($post->ID, 'page_sidebar_menu', TRUE) ? get_post_meta($post->ID, 'page_sidebar_menu', TRUE) : $post->post_name; ?>
 	
+	<h2 class=widgettitle"><? $side_menu ?> </h2>
 	<?=wp_nav_menu(array(
 	'theme_location' => '',
 	'menu' => $side_menu,
 	'container' => 'false',
-	'menu_class' => 'menu '.get_header_styles(),
-	'menu_id' => 'sidebar-menu',
-	'walker' => new Bootstrap_Walker_Nav_Menu()
+	'menu_class' => 'widget widget_nav_menu'.get_header_styles(),
+	'menu_id' => 'sidebar-menu'
 	));
 	?>
 		
