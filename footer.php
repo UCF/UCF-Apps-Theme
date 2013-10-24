b75e649012cb9eab811fd8e315003556d81ff355
Craig Froehlich <craigf@outlook.com>
2013-10-24 16:15:50 -0400

    Fixed bug

--- footer.php
+++ footer.php
@@ -40,9 +40,8 @@
 											<div class = "span3">
 											
 											<?php 
-												$footer_menu = wp_get_nav_menu_object('footer-menu');
-												if ($footer_menu) { ?>
-													<h4><? echo $footer_menu->name ?></h4>
+												if (has_nav_menu('footer-menu')) { ?>
+													<h4>Useful Links</h4>
 													<?=wp_nav_menu(array(
 														'theme_location' => 'footer-menu', 
 														'container' => 'false', 
