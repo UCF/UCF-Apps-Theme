d60ee4528e456ad3257d9fe99be71a6023ce27e1
Craig Froehlich <craigf@outlook.com>
2013-10-24 10:57:01 -0400

    Added header-menu as default on pages without menu

--- sidebar.php
+++ sidebar.php
@@ -2,16 +2,29 @@
 
 <?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Sidebar')):?>
 
-	<?php $side_menu = get_post_meta($post->ID, 'page_sidebar_menu', TRUE) ? get_post_meta($post->ID, 'page_sidebar_menu', TRUE) : $post->post_name; ?>
+	<?php $side_menu = get_post_meta($post->ID, 'page_sidebar_menu', TRUE) ? get_post_meta($post->ID, 'page_sidebar_menu', TRUE) : 'default'; ?>
 	
-	<h2 class=widgettitle"><? echo $side_menu ?> </h2>
-	<?=wp_nav_menu(array(
-	'theme_location' => '',
-	'menu' => $side_menu,
-	'container' => 'false',
-	'menu_class' => 'widget widget_nav_menu'.get_header_styles(),
-	'menu_id' => 'sidebar-menu'
-	));
-	?>
+	<?php 
+		if ($side_menu == 'default' ) { ?>
+		
+		<h2 class="widgettitle">Main Menu</h2>
+		<?=wp_nav_menu(array(
+			'theme_location' => 'header-menu',
+			'container' => 'false',
+			'menu_class' => 'widget widget_nav_menu'.get_header_styles(),
+			'menu_id' => 'sidebar-menu',
+			));
+		?>
+		<?php } else { ?>
+	
+			<h2 class="widgettitle"><? echo $side_menu ?> </h2>
+			<?=wp_nav_menu(array(
+			'theme_location' => '',
+			'menu' => $side_menu,
+			'container' => 'false',
+			'menu_class' => 'widget widget_nav_menu'.get_header_styles(),
+			'menu_id' => 'sidebar-menu'
+			));
+			?>
 		
 <?php endif;?>
